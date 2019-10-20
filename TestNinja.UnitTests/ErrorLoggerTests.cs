21f9aa64742c19fa1fdc2ee37b7fcceb4ff82178
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 13:51:14 +0700

    TestingMethodsThatThrowExceptions

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using _3_6_TestingMethodsThatThrowExceptions.Fundamentals;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
+    }
+}
